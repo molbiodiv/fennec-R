4037b8616561a5ee2cd7c3903b198f7851c2cc14
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-08-04 18:11:57 +0200

    working example and minimal documentation added

--- fennec-v0.01.R
+++ fennec-v0.01.R
@@ -1,26 +1,48 @@
+##########################################################################################
+## Requirements/Settings
+
 library(jsonlite)
 library(phyloseq)
 dbversion = "1.0"
 baseurl = "http://fennec.molecular.eco/api/"
 
+##########################################################################################
+## get list of traits
 list_traits <- fromJSON("http://fennec.molecular.eco/api/listing/traits?dbversion=1.0&limit=1000")
-list_orgs <- fromJSON("http://fennec.molecular.eco/api/listing/organisms?dbversion=1.0&limit=10")
-list_sample <- fromJSON("http://fennec.molecular.eco/api/details/traits?dbversion=1.0&trait_type_id=1&fennec_ids[]=122277&fennec_ids[]=667818")
-
-name2fennid <- fromJSON("http://fennec.molecular.eco/api/mapping/byOrganismName?dbversion=1.0&ids%5B%5D=Bellis%20perennis&ids%5B%5D=Brassica%20napus")
+# list_orgs <- fromJSON("http://fennec.molecular.eco/api/listing/organisms?dbversion=1.0&limit=10")
+# list_sample <- fromJSON("http://fennec.molecular.eco/api/details/traits?dbversion=1.0&trait_type_id=1&fennec_ids[]=122277&fennec_ids[]=667818")
+# name2fennid <- fromJSON("http://fennec.molecular.eco/api/mapping/byOrganismName?dbversion=1.0&ids%5B%5D=Bellis%20perennis&ids%5B%5D=Brassica%20napus")
 # http://fennec.molecular.eco/api/details/traitsOfOrganisms?dbversion=1.0&fennec_ids[]=122277&fennec_ids[]=667818
 
+##########################################################################################
+## mapping scientific names to fennec IDs
+name2fennecID <- function(names){
 
-example_names 		= c("Bellis perennis","Brassica napus")
-example_otutable 	= matrix(nrow=length(example_names),ncol=10,data=sample(c(rep(0,25),1:10),2*10,replace=T))
-rownames(example_otutable) = example_names
-example_traits 		= c("Plant Growth Habit","Leaf Area")
-
-fennec4phyloseq <- function(mat){
+	# prepare the scientific names for the json call	
+	names = unlist(lapply(example_names, URLencode))
+	names = unlist(lapply(names, function(x) paste("&ids%5B%5D=",x, sep="") ))
 	
+	# prepare the call
+	orgCall = paste(names, collapse="")
+	apiCall = paste(baseurl, "mapping/byOrganismName?dbversion=", dbversion, orgCall, sep="")
 	
+	# perform the call and return the result
+	idlist <- fromJSON(apiCall)
+	return(idlist)	
 }
 
+##########################################################################################
+## mapping trait names to fennec IDs
+trait2traitID <- function(traits){
+	traits  = unlist(lapply(traits, function (x) list_traits[list_traits$name == x,"trait_type_id"]))
+
+	## wahts the API for traits by name?
+	#traits = unlist(lapply(example_names, URLencode))
+
+}
+
+##########################################################################################
+## FENNEC mapping of a OTU table in vegan format
 fennec4vegan <- function(mat){
 	taxa = rownames(mat) 
 	fennecIDs 	= as.character(unlist(name2fennecID(taxa)))
@@ -33,13 +55,15 @@ fennec4vegan <- function(mat){
 	apiCallB = paste(baseurl, "details/traitsOfOrganisms?dbversion=", dbversion, idCall, sep="")
 
 	traitlist <- fromJSON(apiCallB)
-
+	#traitlist= traitlist[12]
 	fmat = matrix(ncol=length(traitlist),nrow=length(fennecIDs))
 	colnames(fmat) <- names(traitlist)
 	rownames(fmat) <- fennecIDs
 	
 	#traitdetails=list()
+#	for (i in 1:length(names(traitlist))){
 	for (i in 1:length(names(traitlist))){
+
 		apiCall = paste(baseurl, "details/traits?dbversion=", dbversion, "&trait_type_id=",names(traitlist)[i], idCall, sep="")
 		cat("\n", apiCall)
 		tmptraits <- fromJSON(apiCall)
@@ -57,7 +81,7 @@ fennec4vegan <- function(mat){
 					}			
 				}else{
 					if (length(tmptraits$values[[names(tmptraits$values)[j]]])>0){
-						value 	= as.character(tmptraits$values[[names(tmptraits$values)[j]]])
+						value 	= as.character(tmptraits$values[[names(tmptraits$values)[j]]][1]) ### taking only the first value atm
 						x 		= names(tmptraits$values)[j]
 						fmat[x, names(traitlist)[i]] = value	
 					}		
@@ -88,28 +112,20 @@ fennec4vegan <- function(mat){
 	return(fmat)	
 }
 
+
+##########################################################################################
+## FENNEC mapping of a OTU table in phyloseq format
+fennec4phyloseq <- function(phyloseq){
+	fennec4vegan(t(otu_table(phyloseq)))
+}
+
+
+##########################################################################################
+## Example dataset
+example_names 		= c("Bellis perennis","Brassica napus","Ranunculus acris")
+example_otutable 	= matrix(nrow=length(example_names),ncol=10,data=sample(c(rep(0,25),1:10),length(example_names)*10,replace=T))
+rownames(example_otutable) = example_names
+example_traits 		= c("Plant Growth Habit","Leaf Area")
+
 f = fennec4vegan(example_otutable)
-
-
-name2fennecID <- function(names){
-
-	# prepare the scientific names for the json call	
-	names = unlist(lapply(example_names, URLencode))
-	names = unlist(lapply(names, function(x) paste("&ids%5B%5D=",x, sep="") ))
-	
-	# prepare the call
-	orgCall = paste(names, collapse="")
-	apiCall = paste(baseurl, "mapping/byOrganismName?dbversion=", dbversion, orgCall, sep="")
-	
-	# perform the call and return the result
-	idlist <- fromJSON(apiCall)
-	return(idlist)	
-}
-
-trait2traitID <- function(traits){
-	traits  = unlist(lapply(traits, function (x) list_traits[list_traits$name == x,"trait_type_id"]))
-
-	## wahts the API for traits by name?
-	#traits = unlist(lapply(example_names, URLencode))
-
-}
+f
